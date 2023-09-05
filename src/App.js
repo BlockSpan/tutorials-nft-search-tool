eff38d73b3da1a4b2eb47a8e7b6a9686511d7832
jonaszeutzius <jonas.zeut@gmail.com>
2023-09-05 16:05:49 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NFTSearch from './NFTSearch.js';
+
+function App() {
+
+  return (
+    <div className="App">
+      <NFTSearch/>
+    </div>
+  );
+}
+
+export default App;
